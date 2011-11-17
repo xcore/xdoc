bf4dd8bff88f22a38192a4baff5159c9e4abc403
Dave Lacey <david.lacey@xmos.com>
2011-11-17 13:28:45 +0000

    Added a run latex script

---
+++ xsphinx/run_latex.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import sys
+import subprocess
+import re
+
+cmd = ['pdflatex'] + sys.argv[1:]
+
+n = 1
+while True:
+    print >>sys.stderr, "Running Latex %d" % n
+    n=n+1
+    process = subprocess.Popen(cmd,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT)
+    lines = process.stdout.readlines()
+
+    found_rerun = False
+    for line in lines:
+        #print >>sys.stderr,line
+        if re.match('.*[rR]e-?run.*',line):
+            found_rerun = True
+
+    if not found_rerun:
+        break
+
+for i in range(len(lines)):
+    if lines[i][0] == '!':
+        print >>sys.stderr,'\n'.join(lines[i:i+6])
+
+print ''.join(lines)
