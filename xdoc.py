b60fa948a6bc5745e62bee65ea0f7d4048cd06f4
Dave Lacey <david.lacey@xmos.com>
2012-08-23 09:58:32 +0100

    Fixed build issue with duplicating doxygen dirs

--- xdoc.py
+++ xdoc.py
@@ -119,7 +119,7 @@ def get_config(path,config={}):
 
 def rsync_dir(d,destroot):
     print "Copying %s" % d
-    exclude_pattern = r'.*\.sources.*|.*\.git.*|.*\.zip|.*\.xe|.*\.linked_dirs'
+    exclude_pattern = r'.*\.sources.*|.*\.git.*|.*\.zip|.*\.xe|.*\.linked_dirs|.*\.doxygen|.*\.support.*'
     for root, dirs, files in os.walk(d):
         for f in files:
             src = os.path.join(root, f)
